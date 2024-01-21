0effe6241bb21162062cb79454abd9a24b5b940d
TooniMike <osinjesutooni@gmail.com>
2024-01-21 20:29:15 +0100

    final edit for category widget

--- lib/widgets/category_widget/bloc/category_bloc.dart
+++ lib/widgets/category_widget/bloc/category_bloc.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/foundation.dart';
 import 'package:games_call/repository/game_repository.dart';
 import 'package:games_call/repository/models/genre.dart';
 
@@ -27,7 +26,7 @@ class CategoryBloc extends Bloc<CategoryEvent, CategoryState> {
           categories: genres,
         ),
       );
-    } catch (erroe, stacktrace) {
+    } catch (error, stacktrace) {
       print(stacktrace);
       emit(state.copyWith(status: CategoryStatus.error));
     }
