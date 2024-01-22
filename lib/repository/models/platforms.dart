a48eb992ec8520063a8ad9702f593d5b10daf949
TooniMike <osinjesutooni@gmail.com>
2024-01-22 10:11:06 +0100

    Platforms model

--- lib/repository/models/platforms.dart
+++ lib/repository/models/platforms.dart
@@ -1,22 +1,22 @@
-// import 'package:games_call/repository/models/results.dart';
+import 'package:games_call/repository/models/results.dart';
 
-// class Platforms {
-//   int? count;
-//   String? next;
-//   dynamic previous;
-//   List<Results>? results;
+class Platforms {
+  int? count;
+  String? next;
+  dynamic previous;
+  List<Results>? results;
 
-//   Platforms({
-//     required this.count,
-//     required this.next,
-//     this.previous,
-//     required this.results,
-//   });
+  Platforms({
+    required this.count,
+    required this.next,
+    this.previous,
+    required this.results,
+  });
 
-//    Platforms.fromJson(Map<String, dynamic> json){
-//     count = json['count'];
-//     next = json['next'];
-//     previous = json['previous'];
-//     results = json['results'];
-//   }
-// }
+   Platforms.fromJson(Map<String, dynamic> json){
+    count = json['count'];
+    next = json['next'];
+    previous = json['previous'];
+    results = json['results'];
+  }
+}
