973b364dc7239838adca25ebac8ab201465a93a2
TooniMike <osinjesutooni@gmail.com>
2024-01-25 11:56:27 +0100

    UI Screens completed

--- lib/widgets/games_by_category_widget.dart/games_by_category_widget.dart
+++ lib/widgets/games_by_category_widget.dart/games_by_category_widget.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:games_call/repository/models/result_error.dart';
 import 'package:games_call/widgets/games_by_category_widget.dart/bloc/games_by_category_bloc.dart';
 import 'package:games_call/widgets/games_by_category_widget.dart/games_by_categories_success.dart';
+
+import '../error_widgets.dart';
 
 class GamesByCategory extends StatelessWidget {
   const GamesByCategory({super.key});
@@ -10,12 +11,19 @@ class GamesByCategory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<GamesByCategoryBloc, GamesByCategoryState>(
-      builder: (context, state){
-        return state.status.isSuccess ? GamesByCategorySuccess(
-          categoryName: state.categoryName,
-          games: state.games,
-        ) : state.status.isLoading ? const Center(child: CircularProgressIndicator(),) :state.status.isError ? ErrorGameWidget()
-      }
-    )
+        builder: (context, state) {
+      return state.status.isSuccess
+          ? GamesByCategorySuccess(
+              categoryName: state.categoryName,
+              games: state.games,
+            )
+          : state.status.isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : state.status.isError
+                  ? const ErrorGameWidget()
+                  : const SizedBox();
+    });
   }
 }
