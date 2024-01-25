bc0abd84571812ef465178c110b248d32d9f8b05
TooniMike <osinjesutooni@gmail.com>
2024-01-25 11:53:03 +0100

    error widget

--- lib/widgets/games_by_category_widget.dart/games_by_category_widget.dart
+++ lib/widgets/games_by_category_widget.dart/games_by_category_widget.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:games_call/repository/models/result_error.dart';
+import 'package:games_call/widgets/games_by_category_widget.dart/bloc/games_by_category_bloc.dart';
+import 'package:games_call/widgets/games_by_category_widget.dart/games_by_categories_success.dart';
 
 class GamesByCategory extends StatelessWidget {
   const GamesByCategory({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocBuilder<GamesByCategoryBloc, GamesByCategoryState>(
+      builder: (context, state){
+        return state.status.isSuccess ? GamesByCategorySuccess(
+          categoryName: state.categoryName,
+          games: state.games,
+        ) : state.status.isLoading ? const Center(child: CircularProgressIndicator(),) :state.status.isError ? ErrorGameWidget()
+      }
+    )
   }
 }
